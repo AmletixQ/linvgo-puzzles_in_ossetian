100f7fdb1f6b22aaa262339d6972686ff9859f99
AmletixQ <nazarovamridin308@gmail.com>
2025-07-22 23:48:15 +0300

    Init

---
+++ data/_all_models.py
@@ -0,0 +1,3 @@
+from .db_session import db
+from .users import User
+from .riddles import Riddle
---
+++ data/db_session.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Создаем единственный экземпляр базы данных
+db = SQLAlchemy()
